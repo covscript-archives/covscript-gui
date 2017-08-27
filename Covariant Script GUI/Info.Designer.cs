17ee8800b23c188cd24cba0f2e3e772bf779a2aa
Michael Lee <mikecovlee@163.com>
2017-08-27 23:28:58 +0800

    微调UI

--- Covariant Script GUI/Info.Designer.cs
+++ Covariant Script GUI/Info.Designer.cs
@@ -36,25 +36,26 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.pictureBox1.Image = global::Covariant_Script.Properties.Resources.covariant_script_wide;
             this.pictureBox1.Location = new System.Drawing.Point(12, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(400, 157);
+            this.pictureBox1.Size = new System.Drawing.Size(400, 160);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.Font = new System.Drawing.Font("等线", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label1.Location = new System.Drawing.Point(9, 176);
+            this.label1.Location = new System.Drawing.Point(12, 175);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(363, 65);
+            this.label1.Size = new System.Drawing.Size(400, 80);
             this.label1.TabIndex = 1;
             this.label1.Text = "Covariant Script GUI 1.0.1\r\n版权所有 © 2017 Michael Lee(李登淳)。\r\n该程序自带绝对保证; 详情请参阅<http:" +
     "//www.gnu.org/licenses/>。\r\n这是免费软件，欢迎您在特定条件下重新分发。\r\n请访问<http://covscript.org/>了解更多" +
@@ -64,16 +65,17 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(424, 255);
+            this.ClientSize = new System.Drawing.Size(424, 281);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "InfoForm";
             this.Text = "关于";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
